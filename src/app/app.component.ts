05c0204c7fe16df44235e91029ab0cdb1795cffa
Kolton Bloom <bloomkd46@icloud.com>
2024-03-10 15:50:37 -0700

    Started menu bar implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import defaults from '../assets/resources/_app.json';
+import app from '../assets/resources/app.json';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web-ui';
+  private app: Record<string, any> = app;
+  private defaults = defaults;
+  menubarTitle = this.app['menubarTitle'] ?? this.defaults.menubarTitle;
+  owner = this.app['owner'] || this.defaults.owner;
+  email = this.app['email'] || this.defaults.email;
+  repo = this.app['repo'] || this.defaults.repo;
 }
