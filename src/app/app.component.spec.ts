dc869dad51d488549c125032145ea248377b9a4e
Kolton Bloom <bloomkd46@icloud.com>
2024-03-10 15:50:47 -0700

    Added test support

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import defaults from '../assets/resources/_app.json';
+import config from '../assets/resources/app.json';
 import { AppComponent } from './app.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,16 +24,40 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'web-ui'`, () => {
+  it(`should have an email`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web-ui');
+    if ('email' in config) {
+      expect(app.email).toEqual(config.email);
+    } else {
+      expect(app.email).toEqual(defaults.email);
+    }
   });
-
-  it('should render title', () => {
+  it(`should have a menubar title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('web-ui app is running!');
+    const app = fixture.componentInstance;
+    if ('menubarTitle' in config) {
+      expect(app.menubarTitle).toEqual(config.menubarTitle);
+    } else {
+      expect(app.menubarTitle).toEqual(defaults.menubarTitle);
+    }
+  });
+  it(`should have an owner`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    if ('owner' in config) {
+      expect(app.owner).toEqual(config.owner);
+    } else {
+      expect(app.owner).toEqual(defaults.owner);
+    }
+  });
+  it(`should have a repo`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    if ('repo' in config) {
+      expect(app.repo).toEqual(config.repo);
+    } else {
+      expect(app.repo).toEqual(defaults.repo);
+    }
   });
 });
