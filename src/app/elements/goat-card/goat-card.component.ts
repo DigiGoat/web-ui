b0381d2f0f6f716e4cf694a335f07f4a26bbc328
Kolton Bloom <bloomkd46@icloud.com>
2024-03-14 10:32:34 -0700

    Finished Draft for goats card

---
+++ src/app/elements/goat-card/goat-card.component.ts
@@ -0,0 +1,30 @@
+import { ImageService } from 'src/app/services/image.service';
+
+import { Component, Input, OnInit } from '@angular/core';
+
+
+import type { Goat } from 'src/app/services/goat.service';
+@Component({
+  selector: 'app-goat-card',
+  templateUrl: './goat-card.component.html',
+  styleUrls: ['./goat-card.component.scss']
+})
+export class GoatCardComponent implements OnInit {
+
+  constructor(private imageService: ImageService) { }
+
+  @Input('goat') goat?: Partial<Goat>;
+  name?: string;
+  nickname?: string;
+  description?: string;
+  id?: string;
+  imageSrc?: string;
+  ngOnInit(): void {
+    this.name = this.goat?.name;
+    this.nickname = this.goat?.nickname;
+    this.description = this.goat?.description;
+    this.id = this.goat?.normalizeId;
+    this.imageSrc = this.id ? this.imageService.getImage(this.id) : this.imageService.notFound;
+  }
+  notFound = this.imageService.notFound;
+}
