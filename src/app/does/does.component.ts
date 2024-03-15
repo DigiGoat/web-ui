6ab830c4207bb7959b580a74c67725f381c351b4
Kolton Bloom <bloomkd46@icloud.com>
2024-03-15 09:38:32 -0700

    Changed goats structure to accept the getter function, rather than a type, to make it more dynamic Migrated goatService to use observables instead of promises for compatibility

--- src/app/does/does.component.ts
+++ src/app/does/does.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { GoatService } from '../services/goat.service';
+
+
 @Component({
   selector: 'app-does',
   templateUrl: './does.component.html',
   styleUrls: ['./does.component.scss']
 })
 export class DoesComponent {
-
+  constructor(public goatService: GoatService) { }
 }
--- src/app/elements/goats/goats.component.ts
+++ src/app/elements/goats/goats.component.ts
@@ -1,7 +1,10 @@
 import type { HttpErrorResponse } from '@angular/common/http';
-import { Goat, GoatService } from 'src/app/services/goat.service';
+import { Goat } from 'src/app/services/goat.service';
 
 import { Component, Input, OnInit } from '@angular/core';
+
+
+import type { Observable } from 'rxjs';
 
 
 @Component({
@@ -10,49 +13,25 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./goats.component.scss']
 })
 export class GoatsComponent implements OnInit {
-  public goats: Goat[] = [];
   public err?: HttpErrorResponse;
   public noGoats = false;
-  constructor(private goatService: GoatService) { }
-  @Input() type!: 'Does' | 'Bucks';
+  constructor() { }
+  @Input() goats?: Goat[];
+  @Input() goatGetter?: Observable<Goat[]>;
+  @Input() name?: string;
   ngOnInit() {
-    this.goats = this.getGoats();
-    if (!this.goats.length) {
-      this.loadGoats();
-    }
-  }
-  getGoats() {
-    switch (this.type) {
-      case 'Does':
-        return this.goatService.does;
-      case 'Bucks':
-        return this.goatService.bucks;
-      default:
-        return [];
-    }
-  }
-  loadGoats() {
-    let getter: FunctionProperties<GoatService>;
-    switch (this.type) {
-      case 'Does':
-        getter = 'getDoes';
-        break;
-      case 'Bucks':
-        getter = 'getBucks';
-        break;
-    }
-    if (getter) {
-      this.goatService[getter]().then(goats => {
+    if (!this.goats?.length && this.goatGetter) {
+      this.goatGetter.subscribe({
+        next: goats => {
         if (!goats.length) {
           this.noGoats = true;
         }
         this.goats = goats;
-      }).catch(err => this.err = err);
-    } else {
-      console.error('Something went wrong while retrieving the type of goats to display', this.type);
+        },
+        error: err => this.err = err
+      });
+    } else if (!this.goatGetter) {
+      console.error('No Getter Function Provided, This Is A Software Error');
     }
   }
 }
-type FunctionProperties<T> = {
-  [K in keyof T]: T[K] extends Function ? K : never
-}[keyof T];
