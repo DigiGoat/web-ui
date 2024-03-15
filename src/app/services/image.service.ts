75533ec42264a21a0f4ead95010fa77241af61e0
Kolton Bloom <bloomkd46@icloud.com>
2024-03-15 12:47:32 -0700

    Fixed getImage updating file parameter on imageMap entry

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -14,18 +14,18 @@ export class ImageService {
     const key = Object.keys(this.imageMap).find(directory => searchQueries.includes(directory));
     if (key && this.imageMap[key].length) {
       const image = this.imageMap[key].find(image => image.cover) || this.imageMap[key][0];
-      return `./assets/images/${key}/${image.file}`;
-    } else {
-      return this.notFound;
+      return { ...image, file: `./assets/images/${key}/${image.file}` };
     }
+    return this.NotFound;
   }
 
-  public readonly notFound = './assets/images/ImageNotFound.png';
+  public readonly NotFound: ImageEntry = { file: './assets/images/ImageNotFound.png', description: 'The Requested Image Does Not Exist' };
 }
 type ImageMap = {
-  [directory: string]: {
-    file: string,
-    description?: string;
-    cover?: true;
-  }[];
+  [directory: string]: ImageEntry[];
+};
+export type ImageEntry = {
+  file: string,
+  description?: string;
+  cover?: true;
 };
