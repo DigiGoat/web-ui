c36acfa5d70ede2416c0a1a7e7d8f2fd7ea64de4
Kolton Bloom <bloomkd46@icloud.com>
2024-04-04 13:17:09 -0700

    Updated age pipe to use custom logic instead of `Intl.RelativeTimeFormat` This was a rather easy substitution and may increase browser compatiblity

--- src/app/pipes/age.pipe.ts
+++ src/app/pipes/age.pipe.ts
@@ -5,32 +5,36 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AgePipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  transform(value: any): unknown {
     try {
       if (!Date.parse(value)) {
         throw new Error('Failed to parse date');
       }
-      const date = new Date(value);
-      const dateDays = date.getTime() / (1000 * 3600 * 24);
-      const now = new Date();
-      const nowDays = now.getTime() / (1000 * 3600 * 24);
-      if (date > now) {
+      /** The targeted date in days */
+      const date = Date.parse(value) / (1000 * 3600 * 24);
+      /** The current date in days */
+      const now = Date.now() / (1000 * 3600 * 24);
+      const difference = now - date;
+      if (difference < 0) {
         throw new Error('Provided date is in the future');
       }
-      const formatter = new Intl.RelativeTimeFormat('en-us', { numeric: 'always' });
 
       /** Check if the date is within the past week */
-      if (nowDays - dateDays < 7) {
-        value = formatter.format(dateDays - nowDays, 'days').replace('ago', 'old');
+      if (difference < 7) {
+        const days = Math.trunc(difference);
+        value = `${days} day${days > 1 ? 's' : ''} old`;
         /** Check if the date is within the past 5 weeks */
-      } else if (nowDays - dateDays < 7 * 5) {
-        value = formatter.format(Math.trunc((dateDays - nowDays) / 7), 'weeks').replace('ago', 'old');
+      } else if (difference < 7 * 5) {
+        const weeks = Math.trunc(difference / 7);
+        value = `${weeks} week old`;
         /** Check if the date is within the past year */
-      } else if (nowDays - dateDays < 365) {
-        value = formatter.format(Math.trunc((dateDays - nowDays) / (365 / 12)), 'months').replace('ago', 'old');
+      } else if (difference < 365) {
+        const months = Math.trunc(difference / (365 / 12));
+        value = `${months} month old`;
         /** Everything else is more than 1 year */
       } else {
-        value = formatter.format(Math.trunc((dateDays - nowDays) / 365), 'years').replace('ago', 'old');
+        const years = Math.trunc(difference / 365);
+        value = `${years} year old`;
       }
     } catch (e) {
       console.warn('Failed to parse age from date:', value, 'With error:', e);
