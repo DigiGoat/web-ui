375dbab05efdae085a091a12f4456fa9f8f1b854
Kolton Bloom <bloomkd46@icloud.com>
2024-05-01 08:30:17 -0700

    Set 404 page to `noindex`

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, type OnDestroy, type OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import type { Observable } from 'rxjs';
+import type { Page } from '../../app-routing.module';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
-export class NotFoundComponent {
-
+export class NotFoundComponent implements OnInit, Page, OnDestroy {
+  constructor(private meta: Meta) { }
+  ngOnInit(): void {
+    this.setDescription();
+  }
+  setDescription(): void | Observable<void> {
+    this.meta.addTag({ name: 'robots', content: 'noindex' });
+  }
+  ngOnDestroy(): void {
+    this.meta.removeTag('name="robots"');
+  }
 }
