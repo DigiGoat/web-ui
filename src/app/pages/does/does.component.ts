2f72bb19fc3699f25ad7730e0e31e0c324114a2e
Kolton Bloom <bloomkd46@icloud.com>
2024-05-01 09:08:54 -0700

    Updated Bucks and Does component to meet `Page` requirments

--- src/app/pages/does/does.component.ts
+++ src/app/pages/does/does.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
 
+import type { Observable } from 'rxjs';
+import type { Page } from '../../app-routing.module';
 import { GoatService } from '../../services/goat/goat.service';
 
 
@@ -8,8 +10,14 @@ import { GoatService } from '../../services/goat/goat.service';
   templateUrl: './does.component.html',
   styleUrls: ['./does.component.scss']
 })
-export class DoesComponent {
+export class DoesComponent implements OnInit, Page {
   name = 'Does';
   getter = this.goatService.does;
   constructor(public goatService: GoatService) { }
+  ngOnInit(): void {
+    this.setDescription();
+  }
+  setDescription(): void | Observable<void> {
+    //TODO: throw new Error('Method not implemented.');
+  }
 }
