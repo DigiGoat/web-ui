f6baa06b6a3b44a1ceb230a2a12b10248e744f32
Kolton Bloom <bloomkd46@icloud.com>
2024-05-01 08:38:10 -0700

    Updated homepage to set the meta description tag Only happens if `homeDescription` is set

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
 
+import type { Observable } from 'rxjs';
+import type { Page } from '../../app-routing.module';
 import { ConfigService } from '../../services/config/config.service';
+import { Meta } from '@angular/platform-browser';
 
 
 @Component({
@@ -8,6 +11,16 @@ import { ConfigService } from '../../services/config/config.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  constructor(public config: ConfigService) { }
+export class HomeComponent implements Page, OnInit {
+  public description = '';
+  constructor(public config: ConfigService, private meta: Meta) { }
+  ngOnInit(): void {
+    this.description = this.config.homeDescription;
+    this.setDescription();
+  }
+  setDescription(): void | Observable<void> {
+    if (this.config.homeDescription) {
+      this.meta.addTags([{ name: 'og:description', content: this.description }, { name: 'description', content: this.description }]);
+    }
+  }
 }
