131198da5f6fdea9fd6eff95a2370ad800bc160e
Kolton Bloom <bloomkd46@icloud.com>
2024-05-01 18:24:59 -0700

    added `provideHttpClient(withFetch())` to `app.module.ts`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgOptimizedImage } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgDirective } from './directives/img/img.directive';
 import { GoatCardComponent } from './elements/goat-card/goat-card.component';
 import { GoatsComponent } from './elements/goats/goats.component';
+import { GoatCardComponent as GoatCardModalComponent } from './elements/modal/goat-card/goat-card.component';
+import { ModalComponent } from './elements/modal/modal/modal.component';
+import { NotFoundComponent as NotFoundModalComponent } from './elements/modal/not-found/not-found.component';
+import { BucksComponent } from './pages/bucks/bucks.component';
 import { DoesComponent } from './pages/does/does.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AgePipe } from './pipes/age.pipe';
-import { BucksComponent } from './pages/bucks/bucks.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { ModalComponent } from './elements/modal/modal/modal.component';
-import { GoatCardComponent as GoatCardModalComponent } from './elements/modal/goat-card/goat-card.component';
-import { NotFoundComponent as NotFoundModalComponent } from './elements/modal/not-found/not-found.component';
-import { ImgDirective } from './directives/img/img.directive';
+import { AgePipe } from './pipes/age.pipe';
 
 
 @NgModule({
@@ -40,7 +40,8 @@ import { ImgDirective } from './directives/img/img.directive';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
