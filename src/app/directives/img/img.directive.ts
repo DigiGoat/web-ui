3fdcababee8b4a7356fad6ccff9598a76954ff56
Kolton Bloom <bloomkd46@icloud.com>
2024-05-03 10:43:22 -0700

    Improved img directive Now uses background so that the alt still shows on load failures

--- src/app/directives/img/img.directive.ts
+++ src/app/directives/img/img.directive.ts
@@ -8,14 +8,30 @@ export class ImgDirective implements OnInit {
   constructor(private el: ElementRef<HTMLImageElement>, private imageService: ImageService) { }
 
   ngOnInit() {
-    this.el.nativeElement.classList.add('placeholder', 'border');
-    if (!this.el.nativeElement.parentElement!.classList.contains('placeholder-glow')) {
-      this.el.nativeElement.parentElement!.classList.add('placeholder-wave');
-    }
+    this.placeholder(true);
     this.el.nativeElement.addEventListener('load', () => {
+      this.placeholder(false);
+    });
+    this.el.nativeElement.addEventListener('error', () => {
+      console.warn('Failed to load image from', this.el.nativeElement.src, 'with alt', this.el.nativeElement.alt);
+      this.notFound();
+      this.placeholder(false);
+    }, { once: true });
+  }
+
+  placeholder(placeholder: boolean) {
+    if (placeholder) {
+      this.el.nativeElement.classList.add('placeholder', 'border');
+      if (!this.el.nativeElement.parentElement!.classList.contains('placeholder-glow')) {
+        this.el.nativeElement.parentElement!.classList.add('placeholder-wave');
+      }
+    } else {
       this.el.nativeElement.classList.remove('placeholder', 'border');
       this.el.nativeElement.parentElement!.classList.remove('placeholder-wave', 'placeholder-glow');
-    });
-    this.el.nativeElement.addEventListener('error', () => this.el.nativeElement.src = this.imageService.NotFound.file, { once: true });
+    }
+  }
+  notFound() {
+    this.el.nativeElement.style.backgroundImage = `url('${this.imageService.NotFound.file}')`;
+    this.el.nativeElement.classList.add('not-found');
   }
 }
