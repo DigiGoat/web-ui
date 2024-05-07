bf53d2db91a3c7adb2063b86bc0c648e1975fd8d
Kolton Bloom <bloomkd46@icloud.com>
2024-05-07 07:38:42 -0700

    Updated goat-card tests

--- src/app/elements/goat-card/goat-card.component.spec.ts
+++ src/app/elements/goat-card/goat-card.component.spec.ts
@@ -1,7 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NgOptimizedImage } from '@angular/common';
-import { ImgDirective } from '../../directives/img/img.directive';
 import { AgePipe } from '../../pipes/age.pipe';
 import { ImageService } from '../../services/image/image.service';
 import { GoatCardComponent } from './goat-card.component';
@@ -13,9 +11,8 @@ describe('GoatCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NgOptimizedImage],
-      declarations: [GoatCardComponent, AgePipe, ImgDirective],
-      providers: [ImageService, ImgDirective],
+      declarations: [GoatCardComponent, AgePipe],
+      providers: [ImageService],
     })
       .compileComponents();
     fixture = TestBed.createComponent(GoatCardComponent);
@@ -56,37 +53,9 @@ describe('GoatCardComponent', () => {
     it('should have an image', () => {
       const element = html.querySelector('[test-id="goat-image"]') as HTMLImageElement & { ngSrc: string; };
       expect(element).toBeTruthy();
-      expect(element.classList).toContain('placeholder');
-      expect(element.src).toBe('http://localhost/assets/images/TEST_NICKNAME/TEST_IMAGE_FILE');
+      expect(element.ngSrc).toBe('/assets/images/TEST_NICKNAME/TEST_IMAGE_FILE');
       expect(element.width).toBe(400);
       expect(element.height).toBe(250);
-    });
-    it('should reveal the image once loaded', () => {
-      const element = html.querySelector('[test-id="goat-image"]') as HTMLImageElement;
-      expect(element).toBeTruthy();
-      expect(element.classList).toContain('placeholder');
-      expect(element.parentElement!.classList).toContain('placeholder-wave');
-      element.dispatchEvent(new Event('load'));
-      expect(element.classList).not.toContain('placeholder');
-      expect(element.parentElement!.classList).not.toContain('placeholder-wave');
-    });
-    it('should set image src to NotFound if image fails to load ONCE', async () => {
-      const element = html.querySelector('[test-id="goat-image"]') as HTMLImageElement;
-      expect(element).toBeTruthy();
-      expect(element.classList).toContain('placeholder');
-      expect(element.parentElement!.classList).toContain('placeholder-wave');
-      element.dispatchEvent(new Event('error'));
-      expect(element.style.backgroundImage).toBe('url(/assets/images/ImageNotFound.png)');
-      expect(element.classList).not.toContain('placeholder');
-      expect(element.parentElement!.classList).not.toContain('placeholder-wave');
-    });
-    it('should log image loading failures', () => {
-      const element = html.querySelector('[test-id="goat-image"]') as HTMLImageElement;
-      expect(element).toBeTruthy();
-      const consoleSpy = jest.spyOn(console, 'warn');
-      element.dispatchEvent(new Event('error'));
-      expect(consoleSpy).toHaveBeenCalledTimes(1);
-      expect(consoleSpy).toHaveBeenCalledWith('Failed to load image from', 'http://localhost/assets/images/TEST_NICKNAME/TEST_IMAGE_FILE', 'with alt', 'TEST_IMAGE_ALT');
     });
     it('should have a link', () => {
       const element = html.querySelector('[test-id="goat-link"]') as HTMLAnchorElement & { routerLink: string; };
@@ -117,6 +86,53 @@ describe('GoatCardComponent', () => {
       expect(fixture).toMatchSnapshot();
     });
   });
+  describe('With an empty Goat', () => {
+    let html: HTMLElement;
+    beforeEach(() => {
+      component['goat'] = {};
+      fixture.detectChanges();
+      html = fixture.nativeElement;
+    });
+    it('should exist', () => {
+      const element = html.querySelector('[test-id="goat"]') as HTMLDivElement;
+      expect(element).toBeTruthy();
+    });
+    it('should NOT display the placeholder div', () => {
+      const element = html.querySelector('[test-id="placeholder"]') as HTMLDivElement;
+      expect(element).toBeFalsy();
+    });
+    it('should have an image', () => {
+      const element = html.querySelector('[test-id="goat-image"]') as HTMLImageElement & { ngSrc: string; };
+      expect(element).toBeTruthy();
+      expect(element.ngSrc).toBe('/');
+      expect(element.width).toBe(400);
+      expect(element.height).toBe(250);
+    });
+    it('should have a link', () => {
+      const element = html.querySelector('[test-id="goat-link"]') as HTMLAnchorElement & { routerLink: string; };
+      expect(element).toBeFalsy();
+    });
+    it('should have a name', () => {
+      const element = html.querySelector('[test-id="goat-name"]') as HTMLHeadingElement;
+      expect(element).toBeTruthy();
+      expect(element.innerHTML).toBe('');
+    });
+    it('should have an age', () => {
+      const element = html.querySelector('[test-id="goat-age"]') as HTMLHeadingElement;
+      expect(element).toBeFalsy();
+    });
+    it('should have a nickname', () => {
+      const element = html.querySelector('[test-id="goat-nickname"]') as HTMLHeadingElement;
+      expect(element).toBeFalsy();
+    });
+    it('should have a description', () => {
+      const element = html.querySelector('[test-id="goat-description"]') as HTMLDivElement;
+      expect(element).toBeFalsy();
+    });
+    it('should match the snapshot', () => {
+      expect(fixture).toMatchSnapshot();
+    });
+  });
   describe('Without a Goat', () => {
     let html: HTMLElement;
     beforeEach(() => {
@@ -134,7 +150,6 @@ describe('GoatCardComponent', () => {
     it('should have a placeholder image', () => {
       const element = html.querySelector('[test-id="placeholder-image"]') as HTMLImageElement;
       expect(element).toBeTruthy();
-      expect(element.classList).toContain('placeholder');
       expect(element.parentElement!.classList).toContain('placeholder-glow');
       expect(element.width).toBe(400);
       expect(element.height).toBe(200);
