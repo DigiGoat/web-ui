21fb28d8211f7b2231f407480a4c351e2d62aadd
Kolton Bloom <bloomkd46@icloud.com>
2024-05-08 12:04:27 -0700

    Updated configService mock

--- src/app/services/config/config.service.mock.ts
+++ src/app/services/config/config.service.mock.ts
@@ -1,9 +1,20 @@
 import type { ConfigService } from './config.service';
 
 export const ConfigServiceMock = {
+  homeTitle: 'TEST_TITLE',
+  owner: 'TEST_OWNER',
   email: 'TEST_EMAIL',
   homeDescription: 'TEST_DESCRIPTION',
-  homeTitle: 'TEST_TITLE',
   menubarTitle: 'TEST_MENUBAR_TITLE',
-  owner: 'TEST_OWNER',
+  tabTitle: 'TEST_TAB_TITLE',
+  link: 'TEST_LINK'
+} as ConfigService;
+export const EmptyConfigServiceMock = {
+  homeTitle: '',
+  owner: '',
+  email: '',
+  homeDescription: '',
+  menubarTitle: '',
+  tabTitle: '',
+  link: ''
 } as ConfigService;
