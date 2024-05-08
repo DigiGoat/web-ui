3a5efae7c7dd4c7fb160e0006db0e1bb74262792
Kolton Bloom <bloomkd46@icloud.com>
2024-05-08 09:49:28 -0700

    Added tests for modal > not-found

--- src/app/elements/modal/not-found/not-found.component.spec.ts
+++ src/app/elements/modal/not-found/not-found.component.spec.ts
@@ -10,14 +10,22 @@ describe('NotFoundComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [NotFoundComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
+    component.searchParam = 'TEST';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display a not found message', () => {
+    const element = fixture.nativeElement.querySelector('[test-id=message]');
+    expect(element.innerHTML).toBe('The Requested Goat "TEST" Was Not Found');
+  });
+  it('should match the snapshot', () => {
+    expect(fixture).toMatchSnapshot();
   });
 });
