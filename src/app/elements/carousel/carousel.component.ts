ecf37c791ebc096d556c585dbbc3b5efcb700e17
Kolton Bloom <bloomkd46@icloud.com>
2024-05-08 08:19:08 -0700

    Changed the color of the carousel indicators so that they have more contrast

--- src/app/elements/carousel/carousel.component.ts
+++ src/app/elements/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, type AfterViewInit, type ElementRef, type OnDestroy, type OnInit } from '@angular/core';
+import { Component, Input, ViewChild, ViewEncapsulation, type AfterViewInit, type ElementRef, type OnDestroy, type OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { ConfigService } from '../../services/config/config.service';
 import type { ImageEntry } from '../../services/image/image.service';
@@ -7,12 +7,13 @@ import { PlatformService } from '../../services/platform/platform.service';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrl: './carousel.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CarouselComponent implements AfterViewInit, OnDestroy, OnInit {
   constructor(private platformService: PlatformService, private meta: Meta, private configService: ConfigService) { }
   ngOnInit() {
-    for (const image of this.images ?? []) {
+    for (const image of this.images) {
       this.meta.addTag({ name: 'og:image', content: this.configService.link ? new URL(image.file, this.configService.link).toString() : image.file });
       //this.meta.addTags([{ name: 'og:image:width', content: '400' }, { name: 'og:image:height', content: '200' }], true);
       if (image.alt) {
