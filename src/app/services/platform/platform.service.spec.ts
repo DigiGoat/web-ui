77401747368e03bbc9d571b036f498a76e81f0d3
Kolton Bloom <bloomkd46@icloud.com>
2024-05-08 12:11:57 -0700

    Added unit tests for platform service

--- src/app/services/platform/platform.service.spec.ts
+++ src/app/services/platform/platform.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PlatformService } from './platform.service';
 
 describe('PlatformService', () => {
@@ -12,5 +11,17 @@ describe('PlatformService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have isServer property', () => {
+    expect(service.isServer).toBeDefined();
+  });
+
+  it('should have isBrowser property', () => {
+    expect(service.isBrowser).toBeDefined();
+  });
+
+  it('should have isBot property', () => {
+    expect(service.isBot).toBeDefined();
   });
 });
