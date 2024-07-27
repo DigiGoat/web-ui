99f2495b6c6443e6eb39fcc4db5139438c9ab3ce
Kolton Bloom <bloomkd46@icloud.com>
2024-07-26 19:30:40 -0500

    Better detection for goat's images TODO: update tests

--- src/app/services/image/image.service.ts
+++ src/app/services/image/image.service.ts
@@ -20,13 +20,28 @@ export class ImageService {
     return { ...this.NotFound, file: '/' };
   }
   getImages(searchQueries: (string | undefined)[]) {
-    const key = Object.keys(this.imageMap).find(directory => searchQueries.includes(directory));
-    if (key && this.imageMap[key].length) {
-      const images = this.imageMap[key].map(image => { return { ...image, file: `./assets/images/${key}/${image.file}` }; });
+    const keys = Object.keys(this.imageMap).filter(directory => searchQueries.includes(directory));
+    console.log('keys:', keys);
+    if (keys.length > 1) {
+      const images = [];
+      for (const key of keys) {
+        if (this.imageMap[key].length) {
+          images.push(...this.imageMap[key].map(image => { return { ...image, file: `./assets/images/${key}/${image.file}` }; }));
+        }
+      }
       return images;
+    } else if (keys.length) {
+      const key = keys[0];
+      if (this.imageMap[key].length) {
+        return this.imageMap[key].map(image => { return { ...image, file: `./assets/images/${key}/${image.file}` }; });
+      } else {
+        //Intentionally give an invalid link
+        return [{ ...this.NotFound, file: '/' }];
+      }
+    } else {
+      //Intentionally give an invalid link
+      return [{ ...this.NotFound, file: '/' }];
     }
-    //Intentionally give an invalid link
-    return [{ ...this.NotFound, file: '/' }];
   }
 
   public readonly NotFound: ImageEntry = { file: './assets/images/ImageNotFound.png', alt: 'The Requested Image Does Not Exist' };
