b5e2336bcb32d491c31e9f510e3636994da5ef6e
Kolton Bloom <bloomkd46@icloud.com>
2024-11-24 11:42:03 -0800

    added tooltips to socials

---
+++ src/app/directives/tooltip/tooltip.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef, HostBinding, HostListener, Input, type AfterViewInit, type OnDestroy } from '@angular/core';
+import type { Tooltip } from 'bootstrap';
+import { PlatformService } from '../../services/platform/platform.service';
+
+@Directive({
+  selector: '[tooltip]'
+})
+export class TooltipDirective implements AfterViewInit, OnDestroy {
+  private bsTooltip?: Tooltip;
+  @Input('tooltip-placement') placement: 'auto' | 'top' | 'bottom' | 'left' | 'right' = 'auto';
+  @Input() set tooltip(value: string) {
+    if (this.bsTooltip) {
+      this.bsTooltip.setContent({ '.tooltip-inner': value });
+    } else {
+      this.el.nativeElement.setAttribute('data-bs-title', value);
+    }
+  }
+  @HostBinding('attr.data-bs-toggle') toggle = 'tooltip';
+  @HostListener('click') onClick() {
+    this.bsTooltip?.hide();
+  }
+  constructor(private el: ElementRef, private platformService: PlatformService) { }
+  ngAfterViewInit(): void {
+    if (this.platformService.isBrowser) {
+      this.bsTooltip = bootstrap.Tooltip.getOrCreateInstance(this.el.nativeElement, { placement: this.placement });
+    }
+  }
+  ngOnDestroy(): void {
+    this.bsTooltip?.dispose();
+  }
+
+}
