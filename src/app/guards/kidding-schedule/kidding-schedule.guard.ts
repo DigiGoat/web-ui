2ad09d865faeaa3e89a5a644718813f992fe7744
Kolton Bloom <bloomkd46@icloud.com>
2025-01-06 10:07:00 -0800

    moved kidding schedule guards into subfolder

--- src/app/guards/kidding-schedule/kidding-schedule.guard.ts
+++ src/app/guards/kidding-schedule/kidding-schedule.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { ConfigService } from '../services/config/config.service';
+import { ConfigService } from '../../services/config/config.service';
 
 export const kiddingScheduleGuard: CanActivateFn = () => {
   return inject(ConfigService).kiddingSchedule;
