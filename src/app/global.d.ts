01cfc32ecfeb9b556ba77dd394924555ebb9c21e
Kolton Bloom <bloomkd46@icloud.com>
2025-01-09 10:21:26 -0800

    fixed typescript errors introduced by typescript v5.6 also started to introduce support for node v22

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -1,9 +1,12 @@
 import * as Bootstrap from 'bootstrap';
 
 declare global {
-  const bootstrap: typeof Bootstrap | never;
   interface Window {
     gtag: ((command: 'config' | 'event' | 'set' | 'js', parameter: string | Date, arguments?: Record<string, string | boolean> | string) => void) | never;
     clarity: ((command: 'set', parameter: string, arguments?: Record<string, string> | string) => void) | never;
   }
+}
+
+declare module 'bootstrap' {
+  const bootstrap: typeof Bootstrap;
 }
