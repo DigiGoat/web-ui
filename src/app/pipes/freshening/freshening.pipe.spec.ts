58a479a7fbc95162781caaee912b83c26b6015b6
Kolton Bloom <bloomkd46@icloud.com>
2025-06-12 14:11:45 -0700

    updated tests

--- src/app/pipes/freshening/freshening.pipe.spec.ts
+++ src/app/pipes/freshening/freshening.pipe.spec.ts
@@ -1,8 +1,42 @@
-import { FresheningsPipe } from './freshening.pipe';
+import { FresheningPipe } from './freshening.pipe';
 
-describe('FresheningsPipe', () => {
+describe('FresheningPipe', () => {
+  let pipe: FresheningPipe;
+
+  beforeEach(() => {
+    pipe = new FresheningPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new FresheningsPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return empty string for undefined', () => {
+    expect(pipe.transform(undefined)).toBe('');
+  });
+
+  it('should return empty string for empty string', () => {
+    expect(pipe.transform('')).toBe('');
+  });
+
+  it('should return "1st" for 1', () => {
+    expect(pipe.transform(1)).toBe('1st');
+  });
+
+  it('should return "2nd" for 2', () => {
+    expect(pipe.transform(2)).toBe('2nd');
+  });
+
+  it('should return "3rd" for 3', () => {
+    expect(pipe.transform(3)).toBe('3rd');
+  });
+
+  it('should return "4th" for 4', () => {
+    expect(pipe.transform(4)).toBe('4th');
+  });
+
+  it('should return "11th" for 11', () => {
+    expect(pipe.transform(11)).toBe('11th');
+  });
+
 });
